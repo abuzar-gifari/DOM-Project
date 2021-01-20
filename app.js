68fe57e9c4cf4a95e30a4016a6fbc61f46323fb7
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2021-01-20 20:52:35 +0600

    simple js project done

---
+++ app.js
@@ -0,0 +1,90 @@
+//console.log(document.getElementById('task-title'));
+//console.log(document.getElementById('task-title').id);
+//console.log(document.getElementById('task-title').className);
+
+//change style js
+//document.getElementById('task-title').style.backgroundColor = "black";
+//document.getElementById('task-title').style.color = "white";
+//document.getElementById('task-title').style.padding = "5px";
+
+///document.getElementById("task-title").textContent = "Text List Are Shown";
+/*
+	
+	const TaskTitle = document.getElementById("task-title");
+	TaskTitle.innerText = "My Task";
+	TaskTitle.innerHTML = '<span>Task List</span>';
+
+*/
+
+
+//console.log(document.querySelector("#task-title"));
+//console.log(document.querySelector(".card-title"));
+//console.log(document.querySelector("h5"));
+//document.querySelector('li:last-child').style.color = 'blue';
+//document.querySelector('li:last-child').textContent = 'Hello';
+
+
+//document.getElementsByClassName('collection-item')[1].style.fontSize = "10px";
+//const items = document.getElementsByClassName('collection-item');
+//console.log(items);
+
+//items[0].style.color = "blue";
+/*
+const items = document.querySelectorAll('ul li');
+console.log(items);
+
+items.forEach(function(item){
+	item.style.color = 'red';
+	item.textContent = "hello js";
+});
+*/
+
+
+//Add New li Process
+/*
+const li = document.createElement('li');
+
+li.className = 'collection-item';
+li.textContent = "List Item";
+
+document.querySelector('ul.collection').appendChild(li);
+const link = document.createElement('a');
+link.className = "delete-item secondary-content";
+link.innerHTML = '<i class="fa fa-remove"></i>';
+li.appendChild(link);
+console.log(li);	
+
+*/
+
+
+//Delete one li
+/*
+const listItem = document.querySelectorAll('li');
+listItem[0].remove();
+*/
+
+
+
+/*Day-02 My Works*/
+
+//document.querySelector('.clear-tasks').addEventListener('click',function(e){
+	
+//	console.log('clicked');
+//	e.preventDefault();
+	
+//});
+
+//const clearBtn = document.querySelector('.clear-tasks');
+//clearBtn.addEventListener('dblclick',runEvent);
+//function runEvent(e){
+//	console.log('Event type: ${e.type}');
+//}
+
+
+
+
+
+
+
+
+
